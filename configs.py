eb74ea3b84ded30e04d6bad705698dcf0d1b6d85
Qualeafclover <QUALEAFCLOVER@GMAIL.COM>
2021-05-27 11:49:41 +0800

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,33 @@
+SIMULATION_EXE = 'C:/Users/8051/Desktop/190595E/(testing)beta_simulator_windows/beta_simulator.exe'
+BUFF_VALUE = 'self_driving_car_nanodegree_program'
+PROCESS_NAME = 'beta_simulator.exe'
+RAW_LOG_NAME = 'driving_log.csv'
+MODEL_SAVE = 'model.tf'
+
+ANGLE_REDESTRIBTION = 3
+REFERENCE_NAME = 'reference_log.csv'
+
+CAMERA_ANGLE_VARIATION = 2.0 / 25.0
+
+CROP_LEFT   = 0.00
+CROP_RIGHT  = 0.00
+CROP_TOP    = 0.35
+CROP_BOTTOM = 0.10
+
+RESIZE = (109, 253)
+
+VALUE_RESCALE_MIN = 0.0
+VALUE_RESCALE_MAX = 1.0
+
+SEED            = None
+TEST_PERCENTAGE = 0.0
+
+BATCH 	= 16
+
+EPOCHS        = 10
+WARMUP_EPOCHS = 1
+
+LEARNING_RATE_MAX = 0.0001
+LEARNING_RATE_MIN = LEARNING_RATE_MAX/100
+
+USE_TENSORBOARD = True
