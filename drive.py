cbb30e3862b8e2649a6a1b73536416cc3c02d261
Qualeafclover <QUALEAFCLOVER@GMAIL.COM>
2021-05-27 14:05:59 +0800

    Create drive.py

---
+++ drive.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import socketio
+import eventlet
+import eventlet.wsgi
+from flask import Flask
+
+class PIController:
+    def __init__(self, Kp=0.1, Ki=0.002):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.target = 22 
+        self.integral = 0.0
+
+    def init(self):
+        self.integral = 0.0
+
+    def set_target(self, sp):
+        self.target = sp
+
+    def update(self, measurement):
+        err = self.target - measurement
+        self.integral += err
+        return self.Kp*err + self.Ki*self.integral
+
+class Simulator:
+    def __init__(self, call_back_fn, show=True):
+        self.sio = socketio.Server()
+        self.app = Flask(__name__)
+        self.call_back_fn = call_back_fn
+        self.show_movement = show
+
+    def init(self):
+        @self.sio.on("telemetry")
+        def telemetry(sid, data):
+            steering_angle, throttle = self.call_back_fn(data)
+            if self.show_movement:
+                print("Steering: {}, Throttle: {}".format(steering_angle, throttle))
+            self.send(steering_angle, throttle)
+
+        @self.sio.on("connect")
+        def connect(sid, environ):
+            print("connected", sid)
+            self.send(0, 0)
+
+    def run(self):
+        eventlet.wsgi.server(eventlet.listen(('', 4567)), socketio.Middleware(self.sio, self.app))
+
+    def kill(self):
+        raise eventlet.StopServe
+
+    def send(self, steering_angle, throttle):
+        self.sio.emit("steer",
+             data={ "steering_angle": str(steering_angle),
+                    "throttle": str(throttle) },
+             skip_sid=True)
+
+if __name__ == "__main__":
+    def controller(data):
+        return 0.0, 1.0
+
+    sim = Simulator(controller)
+    sim.init()
+    sim.run()
